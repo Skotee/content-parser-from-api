7cbba43dff566132e30542ee83cd81b7576a832e
Radosław Wojaczek <radexw1@gmail.com>
2026-08-04 21:22:09 +0200

    Update ContentApiParser.Api/Parsing/IContentParser.cs

--- ContentApiParser.Api/Parsing/IContentParser.cs
+++ ContentApiParser.Api/Parsing/IContentParser.cs
@@ -1,5 +1,5 @@
 public interface IContentParser
 {
     ContentType SupportedType { get; }
-    List<Dictionary<string, object?>> Parse(string decodedContent); // zwracany typ to ten sam co w ParseContentResponse.Data — zapobiegnie to dodatkowemu przekształceniu między parserem a odpowiedzią
+    List<Dictionary<string, object?>> Parse(string decodedContent); // the return type is the same as in ParseContentResponse.Data - this will prevent additional transformation between the parser and the response
 }
